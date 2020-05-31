c88c58487183fe12b76144416c0d96f4e62315a3
Chris Farr <ccfarr@gmail.com>
2020-05-31 15:53:23 -0400

    Wrote out analysis table to s3

--- transform_data.py
+++ transform_data.py
@@ -1,8 +1,16 @@
 from pyspark.sql.types import IntegerType, DoubleType
 import pyspark.sql.functions as F
-from ingest_data import create_spark_session
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql import SparkSession
+
+def create_spark_session() -> SparkSession:
+    """ Creates and returns a SparkSession object"""
+
+    spark = SparkSession \
+        .builder \
+        .getOrCreate()
+
+    return spark
 
 def clean_column_name(column_name: str) -> str:
     """ "Cleans" a column name by removing spaces, etc. """
@@ -21,11 +29,11 @@ def clean_column_name(column_name: str) -> str:
     # Return cleaned name
     return clean_name
 
-def clean_countries_of_the_world(spark: SparkSession) -> DataFrame:
+def transform_countries_of_the_world(spark: SparkSession) -> DataFrame:
     """ Changes data types of selected columns and makes name column unique """
 
     # Filename of input file
-    filename = '/Users/chrisfarr/data-eng-capstone/staging/countries_of_the_world.csv'
+    filename = 's3://data-eng-capstone-cf/staging/countries_of_the_world.csv'
 
     # Read into a spark dataframe
     df = spark.read.csv(filename, header=True)
@@ -51,14 +59,14 @@ def clean_countries_of_the_world(spark: SparkSession) -> DataFrame:
     for c in ['country', 'region']:
         df = df.withColumn(c, F.trim(F.col(c)))
 
-    # Return cleaned dataframe
+    # Return transformed dataframe
     return df
 
-def clean_i94_cit_res_data(spark: SparkSession, df_right: DataFrame) -> DataFrame:
+def transform_i94_cit_res_data(spark: SparkSession, df_right: DataFrame) -> DataFrame:
     """ Changes data types of selected columns and makes name column unique """
 
     # Filename of input file
-    filename = '/Users/chrisfarr/data-eng-capstone/staging/i94_cit_res_data.csv'
+    filename = 's3://data-eng-capstone-cf/staging/i94_cit_res_data.csv'
 
     # Read into a spark dataframe
     df = spark.read.csv(filename, header=True)
@@ -100,26 +108,89 @@ def clean_i94_cit_res_data(spark: SparkSession, df_right: DataFrame) -> DataFram
          .when(df.country == 'TRINIDAD AND TOBAGO', 'Trinidad & Tobago')
          .when(df.country == 'TURKS AND CAICOS ISLANDS', 'Turks & Caicos Is')
          .when(df.country == 'WALLIS AND FUTUNA ISLANDS', 'Wallis and Futuna')
+         .when(df.country == 'CHINA, PRC', 'China')
          .otherwise(df.country_join))
 
-    # Return cleaned dataframe
-    return df.join(df_right, df.country_join == df_right.country, how='left') \
-            .select('country_id', df.country, df_right.country.alias('country_fk'))
+    # Define country_fk via left outer join
+    df = df.join(df_right, df.country_join == df_right.country, how='left') \
+           .select('country_id', df.country, df_right.country.alias('country_fk'))
+
+    # Return transformed dataframe
+    return df
+
+def transform_i94_data(spark: SparkSession, df_right: DataFrame) -> DataFrame:
+    """ Transforms the i9_data as outlined below """
+
+    # Filename of input file
+    filename = 's3://data-eng-capstone-cf/staging/i94_data'
+
+    # Read into a spark dataframe
+    df = spark.read.parquet(filename)
+
+    # Keep columns of interest (i94mon, i94res, i94mode, i94bir, i94visa, visatype)
+    cols = ['i94mon', 'i94res', 'i94mode', 'i94bir', 'i94visa', 'visatype']
+    df = df.select(cols)
+
+    # Cast double typed columns as ints (i94mon, i94res, i94mode, i94bir, i94visa)
+    cols.remove('visatype')
+    for col in cols:
+        df = df.withColumn(col, F.col(col).cast(IntegerType()))
+
+    # Decode selected columns using mapping file (i94res)
+    # Include country_fk column from mapping file
+
+    # Define country_fk via left outer join
+    df = df.join(df_right, df.i94res == df_right.country_id, how='left')
+    df = df.drop('country_id')
+
+    # Decode selected columns using F.when (i94mode, i94visa)
+    # i94mode
+    df = df.withColumn('i94mode_desc',
+            F.when(df.i94mode == 1, 'Air')
+             .when(df.i94mode == 2, 'Sea')
+             .when(df.i94mode == 3, 'Land')
+             .when(df.i94mode == 9, 'Not reported')
+             .otherwise('Other'))
+    df = df.drop('i94mode')
+    df = df.withColumnRenamed('i94mode_desc', 'i94mode')
+
+    # i94visa
+    df = df.withColumn('i94visa_desc',
+            F.when(df.i94visa == 1, 'Business')
+             .when(df.i94visa == 2, 'Pleasure')
+             .when(df.i94visa == 3, 'Student')
+             .otherwise('Other'))
+    df = df.drop('i94visa')
+    df = df.withColumnRenamed('i94visa_desc', 'i94visa')
+
+    # Return transformed dataframe
+    return df
 
 def main():
 
     # Get Spark Session object
     spark = create_spark_session()
 
-    # Clean staging/countires_of_the_world.csv 
-    df_cow = clean_countries_of_the_world(spark)
+    # Transform staging/countires_of_the_world.csv 
+    df_cow = transform_countries_of_the_world(spark)
 
-    # Clean staging/i94_cit_res_data.csv
-    df_cr = clean_i94_cit_res_data(spark, df_cow)
-                 
-    # df_cr.show(300, truncate=False)
-    df_cr.filter(F.col('country_fk').isNull()).show(300, truncate=False)
-    print(df_cr.count())
+    # Transform staging/i94_cit_res_data.csv
+    df_cr = transform_i94_cit_res_data(spark, df_cow)
+
+    # Transform staging/i94_data.parquet
+    df_i94 = transform_i94_data(spark, df_cr)
+
+    df = df_i94.filter(~ df_i94.country.contains('MEXICO')) \
+               .groupBy('country', 'country_fk', 'i94mon') \
+               .count()
+
+    df = df.repartition(1)
+
+    df.write.csv(path='s3://data-eng-capstone-cf/production/analysis.csv',
+                 mode='overwrite',
+                 header=True)
+
+    # print(f'*** IT WORKED *** There are {df_i94.count()} columns in staging/i94_data.parquet.')
 
 if __name__ == '__main__':
     main() 
